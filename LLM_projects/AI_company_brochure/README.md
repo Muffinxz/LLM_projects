a89d1b4a04929bc8abade2f9603e49886d53b208
Achref Chakroun <achref.chakroun.enit@gmail.com>
2026-04-20 01:32:20 -0400

    Created new project : GUI with Gradio

---
+++ LLM_projects/AI_company_brochure/README.md
@@ -0,0 +1,30 @@
+
+### Requirements:
+
+you will find the packages needed for this project in requirements.txt
+
+
+### BUSINESS CHALLENGE:
+
+Create a product that builds a Brochure for a company to be used for prospective clients, investors and potential recruits.
+
+We will be provided a company name and their primary website.
+
+And remember: I'm always available if you have problems or ideas! Please do reach out.
+
+## First step: Have GPT-5-nano figure out which links are relevant
+
+### Use a call to gpt-5-nano to read the links on a webpage, and respond in structured JSON.  
+It should decide which links are relevant, and replace relative links such as "/about" with "https://company.com/about".  
+We will use "one shot prompting" in which we provide an example of how it should respond in the prompt.
+
+This is an excellent use case for an LLM. Imagine trying to code this without LLMs by parsing and analyzing the webpage - it would be very hard!
+
+
+## Second step: make the brochure!
+
+Assemble all the details into another prompt to GPT-5-nano
+
+### Stream the answers:
+With a small adjustment, we can change this so that the results stream back from OpenAI,
+with the familiar typewriter animation
---
+++ LLM_projects/AI_company_brochure/brochure_generator.py
@@ -0,0 +1,134 @@
+# imports
+# If these fail, please check you're running from an 'activated' environment with (llms) in the command prompt
+import os
+import json
+from dotenv import load_dotenv
+from IPython.display import Markdown, display, update_display
+from scraper import fetch_website_links, fetch_website_contents
+from openai import OpenAI
+
+#prepare the dotenv that contains the API_KEY
+#If you don't have an openai API_KEY, you can use the ollama model which is free, you need a base_url and the the api_key parameter is "ollama"
+#ollama model is hosted on "http://localhost:11434/v1"
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+if api_key and api_key.startswith('sk-proj-') and len(api_key)>10:
+    print("API key looks good so far")
+else:
+    print("There might be a problem with your API key? Please visit the troubleshooting notebook!")
+    
+MODEL = 'gpt-5-nano'
+openai = OpenAI()
+
+links = fetch_website_links("https://huggingface.co/")
+links
+
+## First step: Have GPT-5-nano figure out which links are relevant
+
+### Use a call to gpt-5-nano to read the links on a webpage, and respond in structured JSON.  
+
+# I am using a one-shot prompting in this example. One-shot prompting is when you give one example of an answer in your prompt.
+#This is a global variable which is not a good practise, but this is to practise on LLM engineering. I am only using the system prompt as a global variable
+link_system_prompt = """
+You are provided with a list of links found on a webpage.
+You are able to decide which of the links would be most relevant to include in a brochure about the company,
+such as links to an About page, or a Company page, or Careers/Jobs pages.
+You should respond in JSON as in this example:
+
+{
+    "links": [
+        {"type": "about page", "url": "https://full.url/goes/here/about"},
+        {"type": "careers page", "url": "https://another.full.url/careers"}
+    ]
+}
+"""
+
+def get_links_user_prompt(url):
+    user_prompt = f"""
+Here is the list of links on the website {url} -
+Please decide which of these are relevant web links for a brochure about the company, 
+respond with the full https URL in JSON format.
+Do not include Terms of Service, Privacy, email links.
+
+Links (some might be relative links):
+
+"""
+    links = fetch_website_links(url)
+    user_prompt += "\n".join(links)
+    return user_prompt
+
+def select_relevant_links(url):
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": link_system_prompt},
+            {"role": "user", "content": get_links_user_prompt(url)}
+        ],
+        response_format={"type": "json_object"}
+    )
+    result = response.choices[0].message.content
+    links = json.loads(result)
+    return links
+
+def select_relevant_links(url):
+    print(f"Selecting relevant links for {url} by calling {MODEL}")
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": link_system_prompt},
+            {"role": "user", "content": get_links_user_prompt(url)}
+        ],
+        response_format={"type": "json_object"}
+    )
+    result = response.choices[0].message.content
+    links = json.loads(result)
+    print(f"Found {len(links['links'])} relevant links")
+    return links
+
+
+## Second step: make the brochure!
+
+###Assemble all the details into another prompt to GPT-5-nano
+
+def fetch_page_and_all_relevant_links(url):
+    contents = fetch_website_contents(url)
+    relevant_links = select_relevant_links(url)
+    result = f"## Landing Page:\n\n{contents}\n## Relevant Links:\n"
+    for link in relevant_links['links']:
+        result += f"\n\n### Link: {link['type']}\n"
+        result += fetch_website_contents(link["url"])
+    return result
+
+
+# Another global variable which is again a system_prompt variable
+
+brochure_system_prompt = """
+You are an assistant that analyzes the contents of several relevant pages from a company website
+and creates a short brochure about the company for prospective customers, investors and recruits.
+Respond in markdown without code blocks.
+Include details of company culture, customers and careers/jobs if you have the information.
+"""
+
+def get_brochure_user_prompt(company_name, url):
+    user_prompt = f"""
+You are looking at a company called: {company_name}
+Here are the contents of its landing page and other relevant pages;
+use this information to build a short brochure of the company in markdown without code blocks.\n\n
+"""
+    user_prompt += fetch_page_and_all_relevant_links(url)
+    user_prompt = user_prompt[:5_000] # Truncate if more than 5,000 characters
+    return user_prompt
+
+
+def create_brochure(company_name, url):
+    response = openai.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "system", "content": brochure_system_prompt},
+            {"role": "user", "content": get_brochure_user_prompt(company_name, url)}
+        ],
+    )
+    result = response.choices[0].message.content
+    display(Markdown(result))
+
